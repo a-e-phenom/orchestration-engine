90135f749a9109a6b845d1b618b48462f15c9597
a-e-phenom <aida.enache-f@phenom.com>
2026-03-02 10:27:05 +0200

    Updated StageDetailMap.tsx

--- src/components/StageDetailMap.tsx
+++ src/components/StageDetailMap.tsx
@@ -22,7 +22,7 @@ const StageDetailMap = ({ stageName, onBack }: StageDetailMapProps) => {
           className="p-2 hover:bg-gray-100 rounded-lg transition-colors flex-shrink-0 -ml-2">
           <ArrowLeft className="w-5 h-5 text-gray-600" />
         </button>
-        <h1 className="text-lg font-semibold text-gray-900 ml-4">{stageName} subflow</h1>
+        <h1 className="text-lg font-semibold text-gray-900 ml-4">{stageName} Subflow</h1>
       </header>
 
       <div className="flex-1 overflow-auto relative" style={{
